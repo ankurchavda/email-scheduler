d68519df0db89fdac8011b1a1b1eb9048dfc5375
Ankur <aachavda0789@gmail.com>
2017-07-15 17:33:42 +0530

    Path Change

--- worker/instance.js
+++ worker/instance.js
@@ -1,7 +1,7 @@
 const child_process = require('child_process');
 
 module.exports.createInstance = function(jsonObj){
-	var worker_process = child_process.fork("C:/PoshaQ/Communication/email-scheduler/index.js");
+	var worker_process = child_process.fork("../scheduler/index.js"); // Path to the scheduler folder
 	worker_process.send(jsonObj);	
 	worker_process.on('close', function (code) {
 		console.log('child process exited with code ' + code);
