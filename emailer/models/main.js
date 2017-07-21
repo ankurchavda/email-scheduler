e3a5dff32106df35bc7e4b09d7667ddf09559a9a
Ankur <aachavda0789@gmail.com>
2017-07-21 20:14:10 +0530

    Get Users with no campaign added

--- emailer/models/main.js
+++ emailer/models/main.js
@@ -1,5 +1,5 @@
 var mongoose = require('mongoose');
-module.exports = function(mon){
+module.exports = function (mon) {
 	var module = {};
 
 	var User = mon.model('User', new mongoose.Schema({
@@ -11,36 +11,86 @@ module.exports = function(mon){
 		retailer: Object,
 	}))
 
-	module.getUsersClick = function(limit, skip, click, callback){
-		console.log("skip: "+skip+" "+"limit: "+limit);
-		User.find({ _id:{$gt: skip}, 'campaignResponse.camp1.click' : click},callback)
-		.limit(limit)
-		.sort('_id')
-		.select({'email': 1, 'campaignResponse': 1}).lean();
+	module.getUsersClick = function (limit, skip, click, callback) {
+		console.log("skip: " + skip + " " + "limit: " + limit);
+		User.find({
+				_id: {
+					$gt: skip
+				},
+				'campaignResponse.camp1.click': click
+			}, callback)
+			.limit(limit)
+			.sort('_id')
+			.select({
+				'email': 1,
+				'campaignResponse': 1
+			}).lean();
 	}
 
-	module.getUsersOpen = function(limit,skip,open,callback){
-		console.log("skip: "+skip+" "+"limit: "+limit);
-		User.find({ _id:{$gt: skip}, 'campaignResponse.camp1.open' : open},callback)
-		.limit(limit)
-		.sort('_id')
-		.select({'email': 1, 'campaignResponse': 1}).lean();	
+	module.getUsersOpen = function (limit, skip, open, callback) {
+		console.log("skip: " + skip + " " + "limit: " + limit);
+		User.find({
+				_id: {
+					$gt: skip
+				},
+				'campaignResponse.camp1.open': open
+			}, callback)
+			.limit(limit)
+			.sort('_id')
+			.select({
+				'email': 1,
+				'campaignResponse': 1
+			}).lean();
 	}
 
-	module.getUserOpenNCLick = function(limit, skip, click, open, callback){
-		console.log("skip: "+skip+" "+"limit: "+limit);
-		User.find({ _id:{$gt: skip}, 'campaignResponse.camp1.click' : click , 'campaignResponse.camp1.open' : open},callback)
-		.limit(limit)
-		.sort('_id')
-		.select({'email': 1, 'campaignResponse': 1}).lean();
+	module.getUserOpenNCLick = function (limit, skip, click, open, callback) {
+		console.log("skip: " + skip + " " + "limit: " + limit);
+		User.find({
+				_id: {
+					$gt: skip
+				},
+				'campaignResponse.camp1.click': click,
+				'campaignResponse.camp1.open': open
+			}, callback)
+			.limit(limit)
+			.sort('_id')
+			.select({
+				'email': 1,
+				'campaignResponse': 1
+			}).lean();
 	}
 
-	module.getUsers= function(limit, skip, callback){
-		console.log("skip: "+skip+" "+"limit: "+limit);
-		User.find({ _id:{$gt: skip}},callback)
-		.limit(limit)
-		.sort('_id')
-		.select({'email': 1, 'campaignResponse': 1}).lean();	
+	module.getUsers = function (limit, skip, callback) {
+		console.log("skip: " + skip + " " + "limit: " + limit);
+		User.find({
+				_id: {
+					$gt: skip
+				}
+			}, callback)
+			.limit(limit)
+			.sort('_id')
+			.select({
+				'email': 1,
+				'campaignResponse': 1
+			}).lean();
+	}
+
+	module.getUsersWithNoCampaign = function (limit, skip, callback) {
+		console.log("skip: " + skip + " " + "limit: " + limit);
+		User.find({
+				_id: {
+					$gt: skip
+				},
+				campaignResponse: {
+					$eq: null
+				}
+			}, callback)
+			.limit(limit)
+			.sort('_id')
+			.select({
+				'email': 1,
+				'campaignResponse': 1
+			}).lean();
 	}
 	return module;
 }
