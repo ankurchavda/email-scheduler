42af3679d6d787fb60390ba0dec6c973899e5a04
Ankur <aachavda0789@gmail.com>
2017-07-25 19:35:30 +0530

    Json object changes

--- worker/index.js
+++ worker/index.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 var instance = require('./instance');
 
 var obj = {
-	array: [1, 3, 4],
+	array: [1, 1],
 	recurrence: {
 		second: 50,
 		minute: null,
@@ -10,42 +10,46 @@ var obj = {
 		dayOfweek: null,
 	},
 	campaign: {
-		sender: "PoshaQ",
+		sender: "StartUp India Yatra",
 		email: "fashion@getposhaq.com",
-		subject: "Worker se aa rella hai bhidu!",
-		title: "Naya!"
+		subject: "{Invitation} StartUp India Yatra Chapter",
+		title: "StartUp India Yatra Campaign"
 	},
 	condition: {
-		click: null,
-		open: null,
-		campaignResponse: null
+		open: {
+			greaterThanEqual: 1,
+			lessThan: 2
+		},
+		campaignSummary: null
 	},
-	path: "../emailer/index.js",
-	retailerId: 4
+	textPath: "../template.txt",
+	htmlPath: "../template.html",
+	path: "../emailer/index.js", // Stay awaayyyyy!
+	retailerId: "4"
 }
 
-var obj2 = {
-	array: [1, 3, 4],
-	recurrence: {
-		second: 20,
-		minute: null,
-		hour: null,
-		dayOfweek: null,
-	},
-	campaign: {
-		sender: "PoshaQ",
-		email: "fashion@getposhaq.com",
-		subject: "Worker se aa rella hai bhidu!",
-		title: "Naya!"
-	},
-	condition: {
-		click: 't',
-		open: 't',
-		campaignResponse: null
-	},
-	path: "../emailer/index.js",
-	retailerId: 4
-}
-
+// var obj2 = {
+// 	array: [1, 3, 4],
+// 	recurrence: {
+// 		second: 20,
+// 		minute: null,
+// 		hour: null,
+// 		dayOfweek: null,
+// 	},
+// 	campaign: {
+// 		sender: "PoshaQ",
+// 		email: "fashion@getposhaq.com",
+// 		subject: "Worker se aa rella hai bhidu!",
+// 		title: "Naya!"
+// 	},
+// 	condition: {
+// 		click: 't',
+// 		open: 't',
+// 		campaignResponse: null
+// 	},
+// 	path: "../emailer/index.js",
+// 	retailerId: 4
+// }
+console.log(process.env.MJ_PUBLIC_KEY);
 instance.createInstance(obj);
 // instance.createInstance(obj2);
