a37bcd1a5dbf4453e83f25fdc40ea0d8ccc4dd78
Ankur <aachavda0789@gmail.com>
2017-08-02 15:48:42 +0530

    Post request added

--- worker/index.js
+++ worker/index.js
@@ -1,55 +1,35 @@
 const fs = require('fs');
 var instance = require('./instance');
+var express = require('express');
+var app = express();
+var fileUpload = require('express-fileupload');
+var bodyParser = require('body-parser');
 
-var obj = {
-	array: [1, 1],
-	recurrence: {
-		second: 50,
-		minute: null,
-		hour: null,
-		dayOfweek: null,
-	},
-	campaign: {
-		sender: "StartUp India Yatra",
-		email: "fashion@getposhaq.com",
-		subject: "{Invitation} StartUp India Yatra Chapter",
-		title: "StartUp India Yatra Campaign"
-	},
-	condition: {
-		open: {
-			greaterThanEqual: 1,
-			lessThan: 2
-		},
-		campaignSummary: null
-	},
-	textPath: "../template.txt",
-	htmlPath: "../template.html",
-	path: "../emailer/index.js", // Stay awaayyyyy!
-	retailerId: "4"
-}
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
+app.use(fileUpload());
+app.set('port', process.env.PORT || 3000);
 
-// var obj2 = {
-// 	array: [1, 3, 4],
-// 	recurrence: {
-// 		second: 20,
-// 		minute: null,
-// 		hour: null,
-// 		dayOfweek: null,
-// 	},
-// 	campaign: {
-// 		sender: "PoshaQ",
-// 		email: "fashion@getposhaq.com",
-// 		subject: "Worker se aa rella hai bhidu!",
-// 		title: "Naya!"
-// 	},
-// 	condition: {
-// 		click: 't',
-// 		open: 't',
-// 		campaignResponse: null
-// 	},
-// 	path: "../emailer/index.js",
-// 	retailerId: 4
-// }
-console.log(process.env.MJ_PUBLIC_KEY);
-instance.createInstance(obj);
-// instance.createInstance(obj2);
+// Enabling Cross Origin Requests
+app.use(function(req, res, next) {
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+	next();
+});
+
+app.post('/start', function(req, res){
+	if(!req.files.seed)
+		res.status(400).send("Please upload the seed file");
+
+	var file = req.files.seed;
+	var obj = JSON.parse(file.data.toString('ascii'));
+	console.log(obj);
+	instance.createInstance(obj);
+	// var json = fs.readFileSync('../seed.json', 'utf8');
+	// console.log(json);
+
+	res.status(200).send("Started....");	
+});
+
+app.listen(app.get('port'));
+console.log("Listening on port "+app.get('port'));
